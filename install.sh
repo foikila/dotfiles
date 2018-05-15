ed3415c61cd0fa54391f2939fb2e488084699b7f
Jonatan Karlsson (lqf433) <jonatan.karlsson@teliacompany.com>
2018-05-15 14:22:13 +0200

    Fix in install script

--- install.sh
+++ install.sh
@@ -4,19 +4,35 @@ if [ -z $1 ]; then
     printf "Usage: ./install.sh [desktop-enviroment]\n"
     exit
 fi 
+
+if [ ! -d $1 ]; then
+    printf "'$1' isn't supported\n"
+    exit
+fi
+
 CP="cp -i"
+
+function install {
+    echo "sudo apt install $@"
+    sudo apt install $@
+}
+
+
+# prog
+
+install kdiff3 clipit git 
+
 
 # DE stuff
 
+install $1 
+
+echo "cd $1"
 cd $1
 
 ./de-install.sh
 
 cd -
-
-# prog
-
-sudo apt install kdiff3 clipit git 
 
 # Configs
 
@@ -25,4 +41,6 @@ $CP bashrc ~/.bashrc
 $CP gitconfig ~/.gitconfig
 
 $CP vimrc ~/.vimrc
-$CP vim/autoload/pathogen.vim ~/.vim/autoload/pathogen.vim 
+$CP vim/autoload/pathogen.vim ~/.vim/autoload/pathogen.vim
+
+
